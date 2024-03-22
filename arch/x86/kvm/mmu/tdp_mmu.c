ff43e509102c2c430291966adbc190357e63c3bc
Lei Wang <lei4.wang@intel.com>
2024-03-22 13:57:58 +0800

    KVM: x86/mmu: Ensure shared pages are included in dirty page logging
    When enabling dirty page logging for a confidential VM, shared pages must have
    the “Shared” bit set in the Guest Frame Number (GFN). For legacy
    (non-confidential) VM guests, the “Shared” bit (i.e., gfn_shared_mask) is
    0, so no functionality impact in this case.
    
    Signed-off-by: Lei Wang <lei4.wang@intel.com>
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -2366,6 +2366,9 @@ static void clear_dirty_pt_masked(struct kvm *kvm, struct kvm_mmu_page *root,
 						   shadow_dirty_mask;
 	struct tdp_iter iter;
 
+	if (!is_private_sp(root))
+		kvm_gfn_to_shared(kvm, gfn);
+
 	lockdep_assert_held_write(&kvm->mmu_lock);
 
 	rcu_read_lock();
