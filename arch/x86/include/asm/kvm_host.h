f40aff9bb9651f6c447c354fc2f052180e7cf0a4
Wei Wang <wei.w.wang@intel.com>
2024-04-16 16:21:00 +0800

    KVM: x86/cgm: Add KVM_CGM_PREPARE ioctl
    KVM_CGM_PREPARE is used by userspace to request the secure firmware (e.g.
    TDX module or SVSM) via KVM to prepare migration (e.g. setting up session
    keys) for a confidential/secure guest. This is usually called before a
    live migration session starts, and the subsequent migration session should
    proceed only when 0 is returned.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1889,6 +1889,8 @@ struct kvm_x86_ops {
 	unsigned long (*vcpu_get_apicv_inhibit_reasons)(struct kvm_vcpu *vcpu);
 
 	gva_t (*get_untagged_addr)(struct kvm_vcpu *vcpu, gva_t gva, unsigned int flags);
+
+	int (*cgm_prepare)(struct kvm *kvm, struct kvm_cgm_prepare *prepare);
 };
 
 struct kvm_x86_nested_ops {
