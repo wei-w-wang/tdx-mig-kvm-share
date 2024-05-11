0a231122fc8100ee6063d7873ff68dbf18a774a9
Wei Wang <wei.w.wang@intel.com>
2024-05-11 15:47:05 +0800

    KVM: TDX_MIG: Add tdx_mig_set_epoch_token()
    Add tdx_mig_set_epoch_token() for callers, on the live migration
    destination, to set an epoch token (received from the source side) to the
    TDX module via the TDH.IMPORT.TRACK seamcall.
    
    Similar to tdx_mig_set_epoch_token(), the buffer that stores the token
    data is pinned during the TDH.IMPORT.TRACK seamcall.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -196,6 +196,7 @@ int tdx_mig_prepare(struct kvm *kvm, struct kvm_cgm_prepare *prepare);
 int tdx_mig_enable_cap(struct kvm *kvm, struct kvm_cap_cgm *cap_cgm);
 int tdx_mig_start(struct kvm *kvm, struct kvm_cgm_data *data);
 int tdx_mig_get_epoch_token(struct kvm *kvm, struct kvm_cgm_data *data);
+int tdx_mig_set_epoch_token(struct kvm *kvm, struct kvm_cgm_data *data);
 #else
 static inline int tdx_hardware_setup(struct kvm_x86_ops *x86_ops) { return -EOPNOTSUPP; }
 static inline void tdx_hardware_unsetup(void) {}
@@ -282,6 +283,12 @@ static inline int tdx_mig_start(struct kvm *kvm, void __user *argp)
 }
 
 static inline int tdx_mig_get_epoch_token(struct kvm *kvm,
+					  struct kvm_cgm_data *data)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int tdx_mig_set_epoch_token(struct kvm *kvm,
 					  struct kvm_cgm_data *data)
 {
 	return -EOPNOTSUPP;
