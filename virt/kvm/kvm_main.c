e4e6b3101ace3767a2852f2d1e2ed096afcb35ef
Wei Wang <wei.w.wang@intel.com>
2024-08-12 15:14:42 +0800

    KVM: Enable dirty page logging for slots with KVM_MEM_GUEST_MEMFD set
    To support guest live migration, userspace requests kvm to perform dirty
    logging for guest memory. Currently, dirty logging is disabled for guest
    memory backed by gmem, which is used for private pages of a confidential
    VM. Since dirty logging for private pages has now been added to tdp_mmu,
    allow the dirty logging option for gmem-based memory slots.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1574,10 +1574,6 @@ static int check_memory_region_flags(struct kvm *kvm,
 	if (kvm_arch_has_private_mem(kvm))
 		valid_flags |= KVM_MEM_GUEST_MEMFD;
 
-	/* Dirty logging private memory is not currently supported. */
-	if (mem->flags & KVM_MEM_GUEST_MEMFD)
-		valid_flags &= ~KVM_MEM_LOG_DIRTY_PAGES;
-
 #ifdef CONFIG_HAVE_KVM_READONLY_MEM
 	/*
 	 * GUEST_MEMFD is incompatible with read-only memslots, as writes to
@@ -2053,9 +2049,6 @@ int __kvm_set_memory_region(struct kvm *kvm,
 		if ((kvm->nr_memslot_pages + npages) < kvm->nr_memslot_pages)
 			return -EINVAL;
 	} else { /* Modify an existing slot. */
-		/* Private memslots are immutable, they can only be deleted. */
-		if (mem->flags & KVM_MEM_GUEST_MEMFD)
-			return -EINVAL;
 		if ((mem->userspace_addr != old->userspace_addr) ||
 		    (npages != old->npages) ||
 		    ((mem->flags ^ old->flags) & KVM_MEM_READONLY))
@@ -2085,6 +2078,9 @@ int __kvm_set_memory_region(struct kvm *kvm,
 	new->flags = mem->flags;
 	new->userspace_addr = mem->userspace_addr;
 	if (mem->flags & KVM_MEM_GUEST_MEMFD) {
+		if (change == KVM_MR_FLAGS_ONLY)
+			kvm_gmem_unbind(old);
+
 		r = kvm_gmem_bind(kvm, new, mem->guest_memfd, mem->guest_memfd_offset);
 		if (r)
 			goto out;
