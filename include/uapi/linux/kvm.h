e70a87e78f2e80b71e4da3bdbb0d713697ba503a
Wei Wang <wei.w.wang@intel.com>
2024-08-12 15:40:29 +0800

    KVM: x86/cgm: Add KVM_CGM_SET_EPOCH_TOKEN ioctl
    KVM_CGM_SET_EPOCH_TOKEN is used by userspace, on the live migration
    destination side, to request the secure firmware (e.g. TDX module) via
    KVM to set an epoch token which is received from the source side via
    KVM_CGM_GET_EPOCH_TOKEN.
    
    Userspace provides a buffer via "struct kvm_cgm_data" to the secure
    firmware via KVM, and the buffer, which has been loaded with the token
    data, will be read by the vendor-specific secure firmware.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1711,4 +1711,6 @@ struct kvm_cgm_data {
 
 #define KVM_CGM_GET_EPOCH_TOKEN _IOWR(KVMIO,  0xd8, struct kvm_cgm_data)
 
+#define KVM_CGM_SET_EPOCH_TOKEN _IOWR(KVMIO,  0xd9, struct kvm_cgm_data)
+
 #endif /* __LINUX_KVM_H */
