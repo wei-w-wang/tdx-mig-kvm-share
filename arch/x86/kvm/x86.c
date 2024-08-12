9f19685bc9e0c66c08a40cf44c1ba4de28c21517
Wei Wang <wei.w.wang@intel.com>
2024-08-12 15:02:54 +0800

    KVM: x86: Check the "cpu_dirty_log" capability based on vm_type
    The current implementation of KVM checks the "cpu_dirty_log" capability
    (e.g., Intel PML) via the global value “kvm_x86_ops.cpu_dirty_log_size”.
    This approach falls short when Intel TDX is enabled. In this case, both
    legacy VMs and TDs share the same global value, even though TDX lacks
    support for the cpu_dirty_log capability while legacy VMs have it.
    
    To address this limitation, the function kvm_cpu_dirty_log_enable() is
    added to check whether the cpu_dirty_log capability is enabled based on
    both the vm_type and kvm_x86_ops.cpu_dirty_log_size. As a result, TD
    guests will have the capability disabled, while legacy VMs can continue
    to operate with the capability enabled.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -6515,6 +6515,12 @@ static int kvm_vm_ioctl_reinject(struct kvm *kvm,
 	return 0;
 }
 
+bool kvm_cpu_dirty_log_enabled(struct kvm *kvm)
+{
+	return kvm->arch.vm_type != KVM_X86_TDX_VM &&
+	       kvm_x86_ops.cpu_dirty_log_size;
+}
+
 void kvm_arch_sync_dirty_log(struct kvm *kvm, struct kvm_memory_slot *memslot)
 {
 
@@ -6527,7 +6533,7 @@ void kvm_arch_sync_dirty_log(struct kvm *kvm, struct kvm_memory_slot *memslot)
 	struct kvm_vcpu *vcpu;
 	unsigned long i;
 
-	if (!kvm_x86_ops.cpu_dirty_log_size)
+	if (!kvm_cpu_dirty_log_enabled(kvm))
 		return;
 
 	kvm_for_each_vcpu(i, vcpu, kvm)
@@ -13083,7 +13089,7 @@ static void kvm_mmu_update_cpu_dirty_logging(struct kvm *kvm, bool enable)
 {
 	int nr_slots;
 
-	if (!kvm_x86_ops.cpu_dirty_log_size)
+	if (!kvm_cpu_dirty_log_enabled(kvm))
 		return;
 
 	nr_slots = atomic_read(&kvm->nr_memslots_dirty_logging);
@@ -13159,7 +13165,7 @@ static void kvm_mmu_slot_apply_flags(struct kvm *kvm,
 		if (READ_ONCE(eager_page_split))
 			kvm_mmu_slot_try_split_huge_pages(kvm, new, PG_LEVEL_4K);
 
-		if (kvm_x86_ops.cpu_dirty_log_size) {
+		if (kvm_cpu_dirty_log_enabled(kvm)) {
 			kvm_mmu_slot_leaf_clear_dirty(kvm, new);
 			kvm_mmu_slot_remove_write_access(kvm, new, PG_LEVEL_2M);
 		} else {
