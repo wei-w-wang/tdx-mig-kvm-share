b1c9787b435418e8fe91222210039470b2aa088d
Wei Wang <wei.w.wang@intel.com>
2024-08-13 16:58:30 +0800

    KVM: TDX: Add seamcall TDH.EXPORT.ABORT
    TDH.EXPORT.ABORT is called on the source platform in TD live migration to
    abort an export session and allows the source TD to resume running. If the
    abort occurs during the pre-copy migration, the @mbmd_info parameter is
    disregarded and can be set to 0. if the abort occurs during the post-copy
    migration, @mbmd_info provides a buffer, which stores an abort token. The
    token is received from the destination platform via the TDH.IMPORT.ABORT
    seamcall.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/tdx_ops.h
+++ arch/x86/kvm/vmx/tdx_ops.h
@@ -734,4 +734,17 @@ static inline u64 tdh_import_end(hpa_t tdr)
 	return seamcall(TDH_IMPORT_END, &in);
 }
 
+static inline u64 tdh_export_abort(hpa_t tdr,
+				   u64 mbmd_info,
+				   u64 mig_stream_info)
+{
+	struct tdx_module_args in = {
+		.rcx = tdr,
+		.r8 = mbmd_info,
+		.r10 = mig_stream_info,
+	};
+
+	return seamcall(TDH_EXPORT_ABORT, &in);
+}
+
 #endif /* __KVM_X86_TDX_OPS_H */
