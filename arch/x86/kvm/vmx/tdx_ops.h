b65e6d75e49c2774276c540678d2f586b6f7f782
Wei Wang <wei.w.wang@intel.com>
2024-08-13 17:04:20 +0800

    KVM: TDX: Add seamcall TDH.EXPORT.RESTORE
    TDH.EXPORT.RESTORE is called on the source platform in TD live migration
    to restore a list of TD private pages' secure EPT entry states after an
    export abort (i.e. TDH.EXPORT.ABORT).
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/tdx_ops.h
+++ arch/x86/kvm/vmx/tdx_ops.h
@@ -747,4 +747,20 @@ static inline u64 tdh_export_abort(hpa_t tdr,
 	return seamcall(TDH_EXPORT_ABORT, &in);
 }
 
+static inline u64 tdh_export_restore(hpa_t tdr,
+				     u64 gpa_list_info,
+				     u64 *rcx)
+{
+	struct tdx_module_args in = {
+		.rcx = gpa_list_info,
+		.rdx = tdr,
+	};
+	u64 ret;
+
+	ret = seamcall_ret(TDH_EXPORT_RESTORE, &in);
+	*rcx = in.rcx;
+
+	return ret;
+}
+
 #endif /* __KVM_X86_TDX_OPS_H */
