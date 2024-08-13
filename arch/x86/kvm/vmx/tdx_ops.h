df5bc49cc0926771d4827f81254d118b814c7982
Wei Wang <wei.w.wang@intel.com>
2024-08-13 10:48:29 +0800

    KVM: TDX: Add seamcalls TDH.EXPORT/IMPORT.STATE.VP
    TDH.EXPORT.STATE.VP exports a bundle of vcpu state data of a paused TD
    vcpu, and TDH.IMPORT.STATE.VP imports a bundle of vcpu state to a target
    TD's vcpu. The state bundle comprises MBMD data (stored in the
    buffer provided by @mbmd_info) and the state data (stored in the buffers
    derived from a gpa list provided by @page_list_info). The state data is
    encrypted using the migration session key during export from the source
    TDX module and decrypted upon import to the destination TDX module.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/tdx_ops.h
+++ arch/x86/kvm/vmx/tdx_ops.h
@@ -682,4 +682,47 @@ static inline u64 tdh_import_state_td(hpa_t tdr,
 
 	return ret;
 }
+
+static inline u64 tdh_export_state_vp(hpa_t tdvpr,
+				      u64 mbmd_info,
+				      u64 page_list_info,
+				      u64 mig_stream_info,
+				      u64 *rdx)
+{
+	struct tdx_module_args in = {
+		.rcx = tdvpr,
+		.r8 = mbmd_info,
+		.r9 = page_list_info,
+		.r10 = mig_stream_info,
+	};
+	u64 ret;
+
+	ret = seamcall_ret(TDH_EXPORT_STATE_VP, &in);
+	*rdx = in.rdx;
+
+	return ret;
+}
+
+static inline u64 tdh_import_state_vp(hpa_t tdvpr,
+				      u64 mbmd_info,
+				      u64 page_list_info,
+				      u64 mig_stream_info,
+				      u64 *rcx,
+				      u64 *rdx)
+{
+	struct tdx_module_args in = {
+		.rcx = tdvpr,
+		.r8 = mbmd_info,
+		.r9 = page_list_info,
+		.r10 = mig_stream_info,
+	};
+	u64 ret;
+
+	ret = seamcall_ret(TDH_IMPORT_STATE_VP, &in);
+	*rcx = in.rcx;
+	*rdx = in.rdx;
+
+	return ret;
+}
+
 #endif /* __KVM_X86_TDX_OPS_H */
