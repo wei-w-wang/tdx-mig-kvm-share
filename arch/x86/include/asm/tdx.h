cc9f3d5e473f0a3f6dd0e540b2bb3f25bce2af16
Wei Wang <wei.w.wang@intel.com>
2024-11-18 14:01:34 +0200

    KVM: TDX: Add seamcalls TDH.EXPORT/IMPORT.STATE.VP
    TDH.EXPORT.STATE.VP exports a bundle of vcpu state data of a paused TD
    vcpu, and TDH.IMPORT.STATE.VP imports a bundle of vcpu state to a target
    TD's vcpu. The state bundle comprises MBMD data (stored in the
    buffer provided by @mbmd_info) and the state data (stored in the buffers
    derived from a gpa list provided by @page_list_info). The state data is
    encrypted using the migration session key during export from the source
    TDX module and decrypted upon import to the destination TDX module.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -212,6 +212,10 @@ u64 tdh_export_state_td(u64 tdr, u64 mbmd_info, u64 page_list_info,
 			u64 mig_stream_info, u64 *rdx);
 u64 tdh_import_state_td(u64 tdr, u64 mbmd_info, u64 page_list_info,
 			u64 mig_stream_info, u64 *rcx, u64 *rdx);
+u64 tdh_export_state_vp(u64 tdvpr, u64 mbmd_info, u64 page_list_info,
+			u64 mig_stream_info, u64 *rdx);
+u64 tdh_import_state_vp(u64 tdvpr, u64 mbmd_info, u64 page_list_info,
+			u64 mig_stream_info, u64 *rcx, u64 *rdx);
 #else
 static inline void tdx_init(void) { }
 static inline int tdx_cpu_enable(void) { return -ENODEV; }
